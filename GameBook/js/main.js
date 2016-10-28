388239f90264ade51e3b9485fe953b13e47ff854
tbreeding <tim.breeding@yahoo.com>
2016-10-28 10:17:57 -0500

    Add files via upload

---
+++ GameBook/js/main.js
@@ -0,0 +1,46 @@
+/*
+Title: Ninja Adventures
+-----------------------
+
+You return home from a week-long trip to find your village has been burned to the ground and your entire clan has been decimated. 
+You find remnants of the battle pointing to the infamous Hamster Clan so you set out after revenge. You must destroy the leader
+of the Hamster Clan before he is able to exact more of his terror on other villages.
+
+----------
+----------
+GAME WORLD
+----------
+----------
+
+
+Characters										Attributes
+----------										----------
+
+ - You (hero)									-name, weapons, attacks
+
+ - Wise Old Man									-Provides guidance, heals you, weapons upgrades
+
+ - Hamster Clan Ninjas							-Basic Enemies to battle
+
+ - Hamster Clan Boss                             -Final Boss, 10x hit points of regular ninjas
+
+ - Village Citizens								-Regular people, provide clues, heals you, or whatever else I can think of
+
+
+----------------------------
+
+Wins and Losses
+---------------
+- Win: Defeat Hamster Clan Boss
+- Lose: Lose in any battle
+
+*/
+
+var main = function() {
+
+	console.log("This is working!");
+
+};
+
+
+$(document).ready(main);
